e6d0cad6842191ac175ef3ca683d6c9e0fa5ef93
ArioElvanda007 <arioelvandavbnet@gmail.com>
2024-07-15 17:47:43 +0200

    FIX error css & JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,9 @@
         @livewireStyles
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('js/app.js') }}">
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
